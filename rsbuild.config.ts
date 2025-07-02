14d63239a6b1e8f15b4374d892095d5d08c40d74
Abdubek <zholay.abdubek@gmail.com>
2025-07-02 16:29:35 +0500

    change vite to rsbuild

---
+++ rsbuild.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginSvgr({
+      svgrOptions: {
+        svgo: true,
+        exportType: "default",
+        namedExport: "ReactComponent",
+        jsxRuntime: "automatic",
+      },
+    }),
+  ],
+  html: {
+    template: "./index.html",
+  },
+  source: {
+    entry: {
+      index: "./src/app/main.tsx",
+    },
+  },
+});
