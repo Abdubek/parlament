b57175e549db07304cbf7d762341739d413a6cf9
Abdubek <zholay.abdubek@gmail.com>
2025-07-11 17:01:15 +0500

    add folder table to sessions module

---
+++ src/modules/documents/sessions/hooks/use-section-params.ts
@@ -0,0 +1,10 @@
+import { sessionsRoute } from "..";
+
+export const useSectionParams = () => {
+  const { _splat } = sessionsRoute.useParams() as { _splat?: string };
+  const splat: string | undefined = _splat;
+  const params = splat?.split("/");
+  const lastSectionId = params?.[params.length - 1];
+
+  return { lastSectionId, params };
+};
